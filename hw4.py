dadb0d6af05bed8c7132fea9e60992688e0b044f
Ivan Kiryanov <kiryanov.i.i@yandex.ru>
2018-10-14 16:53:00 +0300

    final ver hw4 added

--- hw4.py
+++ hw4.py
@@ -6,13 +6,22 @@ import argparse
 ##storage.py --key key_name --val value
 parser = argparse.ArgumentParser()
 parser.add_argument('--key')
-##parser.add_argument('key_name')
 parser.add_argument('--val')
-##parser.add_argument('val_name')
 key = parser.parse_args().key
-val = parser.parse_args().val 
-print(val, key)
+val = str(parser.parse_args().val) 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 dic = dict()
-with open(storage_path, 'w') as f:
-    json.dump(dic, f)
+with open(storage_path, 'r', encoding = 'utf-8') as f:
+    dic = json.load(f)
+if val != 'None':
+    with open(storage_path, 'w', encoding = 'utf-8') as f:         
+        if  key in dic:
+            dic.update({key:dic[key]+', '+ val}) 
+        else:
+            dic.update({key:val}) 
+        f.write(json.dumps(dic, ensure_ascii=False))          
+else:        
+    if key in dic:
+        print(dic[key])
+    else:
+        print('None')
